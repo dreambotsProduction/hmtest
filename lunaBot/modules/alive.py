2895c453d206ba935349e7bdf96c99d4523dde6a
Nonsensexdd <97265462+Nonsensexdd@users.noreply.github.com>
2022-03-09 16:31:45 +0530

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -7,23 +7,23 @@ from pyrogram import __version__ as pyrover
 from lunaBot.events import register as MEMEK
 from lunaBot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
+PHOTO = "https://telegra.ph/file/00952ede1ff26162e0e7e.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "**Holla I'm Luna!** \n\n"
-  LUNA += "🔴 **I'm Working Properly** \n\n"
-  LUNA += "🔴 **My Master : [zeinzo](https://t.me/tdrki_1)** \n\n"
-  LUNA += f"🔴 **Telethon Version : {tlhver}** \n\n"
-  LUNA += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
-  LUNA += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/lunatapibot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/lunaXresso")]]
+  xxx = "**❄𝗛𝗼𝗶𝗶, 𝗜 𝗮𝗺 ⏤‌★GODZILLA☆「🇮🇳」𝗥𝗼𝗯𝗼𝘁!!❄ !** \n\n"
+  xxx += "🔴 **❄ ✘ 𝗜'𝗺 𝗪𝗼𝗿𝗸𝗶𝗻𝗴 𝗶𝗻 𝗪𝗲𝗹𝗹 𝗠𝗮𝗻𝗻𝗲𝗿!** \n\n"
+  xxx += "🔴 ** ❄ ✘ 𝗛𝘂𝗶 𝗛𝘂𝗶 𝗠𝘆 𝗠𝗮𝘀𝘁𝗲𝗿 : [AJAY](https://t.me/XXXTENTACION_FOREVER)** \n\n"
+  xxx += f"🔴 ** ❄ ✘ 𝗧𝗲𝗹𝗲𝘁𝗵𝗼𝗻 𝗩𝗲𝗿𝘀𝗶𝗼𝗻: {tlhver}** \n\n"
+  xxx += f"🔴 **❄ ✘ 𝗣𝘆𝗿𝗼𝗴𝗿𝗮𝗺 𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : {pyrover}** \n\n"
+  xxx += "**❄「𝗧𝗵𝗮𝗻𝘅𝘅 𝗙𝗼𝗿 𝗨𝘀𝗶𝗻𝗴 𝗠𝗲 𝘀𝘂𝗿」❤️❄**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/godzilla_x_robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/godzilla_chatting")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
 async def reload(event):
   tai = event.sender.first_name
   LUNA = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
-  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/zeinproject")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
+  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/insane_bots")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=godzilla,  buttons=BUTTON)
