91dfbdb4dbe886ee87fe2666b788125e9de1d4d6
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-10 12:37:24 +0530

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -1,29 +1,24 @@
-import re
 import os
-
+import re
+from platform import python_version as kontol
 from telethon import events, Button
+from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from lunaBot.events import register as MEMEK
+from lunaBot.events import register
 from lunaBot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/00952ede1ff26162e0e7e.jpg"
 
-@MEMEK(pattern=("/alive"))
+PHOTO = "https://telegra.ph/file/ba62d1cd0445adea68cde.jpg"
+
+@register(pattern=("/alive"))
 async def awake(event):
-  tai = event.sender.first_name
-  xxx = "**❄𝗛𝗼𝗶𝗶, 𝗜 𝗮𝗺 ⏤‌★GODZILLA☆「🇮🇳」𝗥𝗼𝗯𝗼𝘁!!❄ !** \n\n"
-  xxx += "🔴 **❄ ✘ 𝗜'𝗺 𝗪𝗼𝗿𝗸𝗶𝗻𝗴 𝗶𝗻 𝗪𝗲𝗹𝗹 𝗠𝗮𝗻𝗻𝗲𝗿!** \n\n"
-  xxx += "🔴 ** ❄ ✘ 𝗛𝘂𝗶 𝗛𝘂𝗶 𝗠𝘆 𝗠𝗮𝘀𝘁𝗲𝗿 : [AJAY](https://t.me/XXXTENTACION_FOREVER)** \n\n"
-  xxx += f"🔴 ** ❄ ✘ 𝗧𝗲𝗹𝗲𝘁𝗵𝗼𝗻 𝗩𝗲𝗿𝘀𝗶𝗼𝗻: {tlhver}** \n\n"
-  xxx += f"🔴 **❄ ✘ 𝗣𝘆𝗿𝗼𝗴𝗿𝗮𝗺 𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : {pyrover}** \n\n"
-  xxx += "**❄「𝗧𝗵𝗮𝗻𝘅𝘅 𝗙𝗼𝗿 𝗨𝘀𝗶𝗻𝗴 𝗠𝗲 𝘀𝘂𝗿」❤️❄**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/godzilla_x_robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/godzilla_chatting")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=godzilla,  buttons=BUTTON)
-
-@MEMEK(pattern=("/reload"))
-async def reload(event):
-  tai = event.sender.first_name
-  LUNA = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
-  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/insane_bots")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=godzilla,  buttons=BUTTON)
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Godzilla Robot.** \n\n"
+  TEXT += "⚪ **I'm Working Properly** \n\n"
+  TEXT += f"⚪ **My Master : [Xxx](https://t.me/XXXTENTACION_FOREVER)** \n\n"
+  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
+  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/GODZILLA_X_ROBOT?start=help"), Button.url("Support", "https://t.me/godzilla_chatting")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
